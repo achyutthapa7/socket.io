e1a08329c36c6b9aa0e8afea9353711f85fb27d4
Achyut Thapa <126256577+achyutthapa7@users.noreply.github.com>
2025-03-20 11:09:21 +0545

    removed /

--- dist/server.js
+++ dist/server.js
@@ -3,33 +3,29 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import cors from "cors";
 const app = express();
-app.use(cors({
-    origin: [
-        "http://localhost:3000",
-        "https://blog-post-phi-seven.vercel.app/",
-    ], // Replace with your frontend URL
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://blog-post-phi-seven.vercel.app"],
     methods: ["GET", "POST"],
-}));
+  })
+);
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-    cors: {
-        origin: [
-            "http://localhost:3000",
-            "https://blog-post-phi-seven.vercel.app/",
-        ],
-        methods: ["GET", "POST"],
-    },
+  cors: {
+    origin: ["http://localhost:3000", "https://blog-post-phi-seven.vercel.app"],
+    methods: ["GET", "POST"],
+  },
 });
 io.on("connection", (socket) => {
-    console.log("New client connected", socket.id);
-    socket.on("new-blog", (blog) => {
-        socket.broadcast.emit("new-blog", blog);
-    });
+  console.log("New client connected", socket.id);
+  socket.on("new-blog", (blog) => {
+    socket.broadcast.emit("new-blog", blog);
+  });
 });
 app.get("/", (req, res) => {
-    res.send("Server is running");
+  res.send("Server is running");
 });
 const PORT = 5002;
 httpServer.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
